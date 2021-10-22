e6fc2f8385b4a6792f72c8d02d3ef2a83b478efe
andrer54 <andrer54@gmail.com>
2021-10-21 21:25:36 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react-login-with-github
+
+front-end web da aplicação  
+
+utilizando ReactJS.  
+Vite(uma ferramenta performática) TypeScript e CSS modules.  
+Com Flexbox e Grid System,  
+animações utilizando Framer Motion.  
---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+export function LoginBox(){
+    return (
+        
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href="#" className={styles.signInWithGithub}></a>
+        </div>
+    )
+}
